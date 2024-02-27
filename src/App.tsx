3fa09a6165bc14415e7125964405629939dea2aa
Mohammad Haider <arzhdr@gmail.com>
2024-02-27 14:11:04 -0500

    Made Layout Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav nav"  "main"`, //we set how it will look in mobile
+        lg: `"nav nav" "aside main"`, // how it will look in laptops or PCs
+      }}
+    >
+      <GridItem area="nav" bg="crimson">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blueviolet">
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="yellowgreen">
+          Main
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 export default App;
