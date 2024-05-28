9aa48bab9ce8d3260ea1739a8026bd79e58433d2
Mohammad Haider <arzhdr@gmail.com>
2024-05-28 15:32:57 -0400

    Added Sorting function to games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,7 +3,6 @@ import useGames from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-
 import { GameQuery } from "../App";
 
 interface Props {
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,6 +21,7 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.genre?.id,
+        ordering: gameQuery.sortOrder,
       },
     },
     [gameQuery]
