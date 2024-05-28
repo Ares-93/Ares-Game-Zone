0c6f9a6f7348d82e7b67a063e5fb2c8cef6ac633
Mohammad Haider <arzhdr@gmail.com>
2024-05-27 20:20:30 -0400

    Displayed Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
