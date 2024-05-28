0b1b0d18692cfded3a07dbc2dc7d664a71434633
Mohammad Haider <arzhdr@gmail.com>
2024-05-28 15:56:06 -0400

    Created Search Input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import Logo from "../assets/AresGameZone.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={Logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack> //stacks
   );
